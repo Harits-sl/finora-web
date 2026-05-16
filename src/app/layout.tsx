5af60bf8789506680420c33adce7156a8efb30fc
Harits_sl <harits.salsabil@gmail.com>
2026-05-16 23:10:20 +0700

    remove unnecessary import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { PageContainer } from '@/shared/components/page-container';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
